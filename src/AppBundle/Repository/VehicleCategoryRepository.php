6c8c999b1de6db1bb3ea8a5cc1e69e8b25f55c0a
Wils Iglesias <wiglesias83@gmail.com>
2017-03-15 12:22:26 +0100

    add findEnabledSortedByName method in VehicleCategoryRepository

--- src/AppBundle/Repository/VehicleCategoryRepository.php
+++ src/AppBundle/Repository/VehicleCategoryRepository.php
@@ -3,6 +3,8 @@
 namespace AppBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query;
+use Doctrine\ORM\QueryBuilder;
 
 /**
  * Class VehicleCategoryRepository.
@@ -13,4 +15,31 @@ use Doctrine\ORM\EntityRepository;
  */
 class VehicleCategoryRepository extends EntityRepository
 {
+    /**
+     * @return QueryBuilder
+     */
+    public function findEnabledSortedByNameQB()
+    {
+        return $this->createQueryBuilder('vc')
+            ->where('vc.enabled = :value')
+            ->setParameter('value', true)
+            ->orderBy('vc.name', 'ASC')
+        ;
+    }
+
+    /**
+     * @return Query
+     */
+    public function findEnabledSortedByNameQ()
+    {
+        return $this->findEnabledSortedByNameQB()->getQuery();
+    }
+
+    /**
+     * @return array
+     */
+    public function findEnabledSortedByName()
+    {
+        return $this->findEnabledSortedByNameQ()->getResult();
+    }
 }
