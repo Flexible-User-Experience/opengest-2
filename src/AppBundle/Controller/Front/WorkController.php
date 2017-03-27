e95a7adbe0c11dbee7ad42b7b4125c8830b182c5
Wils Iglesias <wiglesias83@gmail.com>
2017-03-27 12:49:59 +0200

    create work detail view

--- src/AppBundle/Controller/Front/WorkController.php
+++ src/AppBundle/Controller/Front/WorkController.php
@@ -8,6 +8,7 @@
 
 namespace AppBundle\Controller\Front;
 
+use Doctrine\ORM\EntityNotFoundException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,12 +20,34 @@ class WorkController extends Controller
      *
      * @return Response
      */
-    public function workAction()
+    public function listAction()
     {
         $works = $this->getDoctrine()->getRepository('AppBundle:Work')->findEnabledSortedByName();
 
         return $this->render(':Frontend:works.html.twig', [
             'works' => $works,
+        ]);
+    }
+
+    /**
+     * @Route("/trabajo/{slug}", name="front_work_detail")
+     *
+     * @param $slug
+     *
+     * @return Response
+     *
+     * @throws EntityNotFoundException
+     */
+    public function detailAction($slug)
+    {
+        $work = $this->getDoctrine()->getRepository('AppBundle:Work')->findOneBy(['slug' => $slug]);
+
+        if (!$work) {
+            throw new EntityNotFoundException();
+        }
+
+        return $this->render(':Frontend:work_detail.html.twig', [
+            'work' => $work,
         ]);
     }
 }
