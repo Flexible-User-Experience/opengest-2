79526e2d60915cf2ee70e4af0e13553606b852d7
Wils Iglesias <wiglesias83@gmail.com>
2017-04-11 11:11:53 +0200

    created import vehicle command

---
+++ src/AppBundle/Command/ImportVehicleCsvCommand.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace AppBundle\Command;
+
+use AppBundle\Entity\Vehicle;
+use Symfony\Component\Console\Exception\InvalidArgumentException;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class ImportVehicleCsvCommand.
+ */
+class ImportVehicleCsvCommand extends AbstractBaseCommand
+{
+    /**
+     * Configure.
+     */
+    protected function configure()
+    {
+        $this->setName('app:import:vehicle');
+        $this->setDescription('Import vehicle from CSV file');
+        $this->addArgument('filename', InputArgument::REQUIRED, 'CSV file to import');
+    }
+
+    /**
+     * Execute.
+     *
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return int|null|void
+     *
+     * @throws InvalidArgumentException
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // Welcome & Initialization & File validations
+        $fr = $this->initialValidation($input, $output);
+
+        // Import CSV rows
+        $beginTimestamp = new \DateTime();
+        $rowsRead = 0;
+        $newRecords = 0;
+        while (($row = $this->readRow($fr)) !== false) {
+            $vehicle = $this->em->getRepository('AppBundle:Vehicle')->findOneBy(['name' => $this->readColumn(9, $row)]);
+            // new vehicle
+            if (!$vehicle) {
+                $vehicle = new Vehicle();
+                ++$newRecords;
+            }
+            // update vehicle
+            $vehicle
+                ->setName($this->readColumn(9, $row))
+                ->setDescription($this->readColumn(11, $row))
+                ->setShortDescription($this->readColumn(10, $row))
+                ->setMainImage($this->readColumn(3, $row))
+                ->setAttatchmentPDF($this->readColumn(14, $row))
+                ->setLink($this->readColumn(18, $row))
+            ;
+            $vehicleCategory = $this->em->getRepository('AppBundle:VehicleCategory')->findOneBy(['name' => $this->readColumn(26, $row)]);
+            if ($vehicleCategory) {
+                $vehicle->setCategory($vehicleCategory);
+            }
+
+            $this->em->persist($vehicle);
+            ++$rowsRead;
+        }
+
+        $this->em->flush();
+        $endTimestamp = new \DateTime();
+        // Print totals
+        $this->printTotals($output, $rowsRead, $newRecords, $beginTimestamp, $endTimestamp);
+    }
+}
