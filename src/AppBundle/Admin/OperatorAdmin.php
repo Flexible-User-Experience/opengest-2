51a577966bda0c78db3e77b704c9bb5eeaa6b0c1
Wils Iglesias <wiglesias83@gmail.com>
2017-08-08 14:31:06 +0200

    add OperatorAdmin backend

---
+++ src/AppBundle/Admin/OperatorAdmin.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+
+/**
+ * Class OperatorAdmin.
+ *
+ * @author Wils Iglesias <wiglesias83@gmail.com>
+ */
+class OperatorAdmin extends AbstractBaseAdmin
+{
+    protected $classnameLabel = 'Operadors';
+    protected $baseRoutePattern = 'administracio/operador';
+    protected $datagridValues = array(
+        '_sort_by' => 'name',
+        '_sort_order' => 'asc',
+    );
+
+    /**
+     * Configure route collection.
+     *
+     * @param RouteCollection $collection
+     */
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        parent::configureRoutes($collection);
+        $collection->remove('delete');
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        unset($this->listModes['mosaic']);
+        $listMapper
+            ->add(
+                'taxIdentificationNumber',
+                null,
+                array(
+                    'label' => 'DNI',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'name',
+                null,
+                array(
+                    'label' => 'Nom',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'surname1',
+                null,
+                array(
+                    'label' => 'Primer cognom',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'enabled',
+                null,
+                array(
+                    'label' => 'Actiu',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                '_action',
+                'actions',
+                array(
+                    'actions' => array(
+                        'show' => array('template' => '::Admin/Buttons/list__action_show_button.html.twig'),
+                        'edit' => array('template' => '::Admin/Buttons/list__action_edit_button.html.twig'),
+//                        'delete' => array('template' => '::Admin/Buttons/list__action_delete_button.html.twig'),
+                    ),
+                    'label' => 'Accions',
+                )
+            )
+        ;
+    }
+}
