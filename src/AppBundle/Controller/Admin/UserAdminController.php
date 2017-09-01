1d773ab6fe73ab5f122eff4fa936006f46fbe307
Wils Iglesias <wiglesias83@gmail.com>
2017-09-01 13:30:21 +0200

    add profile view in Admin

--- src/AppBundle/Controller/Admin/UserAdminController.php
+++ src/AppBundle/Controller/Admin/UserAdminController.php
@@ -2,7 +2,8 @@
 
 namespace AppBundle\Controller\Admin;
 
-use AppBundle\Entity\ContactMessage;
+use AppBundle\Entity\User;
+use AppBundle\Form\UserDefaultEnterpriseForm;
 use Symfony\Component\HttpFoundation\File\Exception\AccessDeniedException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,45 +21,30 @@ class UserAdminController extends BaseAdminController
     /**
      * Profile action.
      *
-     * @param int|string|null $id
-     * @param Request         $request
+     * @param Request $request
      *
      * @return Response
      *
      * @throws NotFoundHttpException If the object does not exist
      * @throws AccessDeniedException If access is not granted
      */
-    public function profileAction($id = null, Request $request = null)
+    public function profileAction(Request $request = null)
     {
         $request = $this->resolveRequest($request);
-        $id = $request->get($this->admin->getIdParameter());
 
-        /** @var ContactMessage $object */
-        $object = $this->admin->getObject($id);
+        /** @var User $object */
+        $object = $this->getUser();
 
-        if (!$object) {
-            throw $this->createNotFoundException(sprintf('unable to find the object with id : %s', $id));
-        }
-
-        $object->setChecked(true);
-        $this->admin->checkAccess('show', $object);
-
-        $preResponse = $this->preShow($request, $object);
-        if ($preResponse !== null) {
-            return $preResponse;
-        }
-
-        $this->admin->setSubject($object);
-
-        $em = $this->getDoctrine()->getManager();
-        $em->flush();
+        $form = $this->createForm(UserDefaultEnterpriseForm::class, $object);
+        $form->handleRequest($request);
 
         return $this->render(
-            $this->admin->getTemplate('show'),
+            '::Admin/User/profile.html.twig',
             array(
                 'action' => 'show',
                 'object' => $object,
                 'elements' => $this->admin->getShow(),
+                'profileForm' => $form->createView(),
             )
         );
     }
