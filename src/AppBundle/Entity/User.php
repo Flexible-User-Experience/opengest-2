2663c1b2072ddd84b162e419a2085da47bb5d1d9
Wils Iglesias <wiglesias83@gmail.com>
2017-09-05 11:51:49 +0200

    add attribute mainImage in user entity

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -2,6 +2,9 @@
 
 namespace AppBundle\Entity;
 
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
 use Sonata\UserBundle\Entity\BaseUser as BaseUser;
 
@@ -14,6 +17,7 @@ use Sonata\UserBundle\Entity\BaseUser as BaseUser;
  *
  * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
  * @ORM\Table(name="admin_user")
+ * @Vich\Uploadable()
  */
 class User extends BaseUser
 {
@@ -25,6 +29,25 @@ class User extends BaseUser
      * @ORM\Column(type="integer")
      */
     protected $id;
+
+    /**
+     * @var File
+     *
+     * @Vich\UploadableField(mapping="user", fileNameProperty="mainImage")
+     * @Assert\File(
+     *     maxSize="10M",
+     *     mimeTypes={"image/jpg", "image/jpeg", "image/png", "image/gif"}
+     * )
+     * @Assert\Image(minWidth=1200)
+     */
+    private $mainImageFile;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     */
+    private $mainImage;
 
     /**
      * @var Enterprise
@@ -45,6 +68,51 @@ class User extends BaseUser
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * @return File
+     */
+    public function getMainImageFile()
+    {
+        return $this->mainImageFile;
+    }
+
+    /**
+     * @param File|null $mainImageFile
+     *
+     * @return User
+     */
+    public function setMainImageFile(File $mainImageFile = null)
+    {
+        $this->mainImageFile = $mainImageFile;
+        if ($mainImageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTime();
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMainImage()
+    {
+        return $this->mainImage;
+    }
+
+    /**
+     * @param string $mainImage
+     *
+     * @return User
+     */
+    public function setMainImage($mainImage)
+    {
+        $this->mainImage = $mainImage;
+
+        return $this;
     }
 
     /**
