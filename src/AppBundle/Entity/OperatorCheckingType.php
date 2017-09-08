e881a45ae08a2dd73c5d4a7f782a27d7c0d5f22e
Wils Iglesias <wiglesias83@gmail.com>
2017-09-08 11:02:57 +0200

    add OperatorCheckingType entity

---
+++ src/AppBundle/Entity/OperatorCheckingType.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use AppBundle\Entity\Traits\DescriptionTrait;
+use AppBundle\Entity\Traits\NameTrait;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class OperatorCheckingType.
+ *
+ * @category Entity
+ *
+ * @author   Wils Iglesias <wiglesias83@gmail.com>
+ * @ORM\Entity
+ * @ORM\Table(name="operator_checking_type")
+ */
+class OperatorCheckingType extends AbstractBase
+{
+    use NameTrait;
+    use DescriptionTrait;
+}
