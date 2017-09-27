1b3e91b385667d3efcf42ac03855a098b053a117
Wils Iglesias <wiglesias83@gmail.com>
2017-09-27 18:23:20 +0200

    add block OperatorChecking

--- src/AppBundle/Block/OperatorCheckingBlock.php
+++ src/AppBundle/Block/OperatorCheckingBlock.php
@@ -2,47 +2,51 @@
 
 namespace AppBundle\Block;
 
-use Doctrine\ORM\EntityManager;
+use AppBundle\Repository\OperatorCheckingRepository;
 use Sonata\BlockBundle\Block\BlockContextInterface;
 use Sonata\BlockBundle\Block\Service\AbstractBlockService;
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 
 /**
- * Class ContactMessageBlock.
+ * Class OperatorCheckingBlock.
  *
  * @category Block
- *
- * @author Wils Iglesias <wiglesias83@gmail.com>
  */
-class ContactMessageBlock extends AbstractBlockService
+class OperatorCheckingBlock extends AbstractBlockService
 {
     /**
-     * @var EntityManager
+     * @var OperatorCheckingRepository
      */
-    private $em;
+    private $ocr;
+
+    /**
+     * @var TokenStorage
+     */
+    private $tss;
 
     /**
      * Methods.
      */
 
     /**
-     * Constructor.
+     * OperatorCheckingBlock constructor.
      *
-     * @param $name
-     * @param EngineInterface $templating
-     * @param EntityManager   $em
+     * @param null|string                $name
+     * @param EngineInterface            $templating
+     * @param OperatorCheckingRepository $ocr
+     * @param TokenStorage               $tss
      */
-    public function __construct($name, EngineInterface $templating, EntityManager $em)
+    public function __construct($name, EngineInterface $templating, OperatorCheckingRepository $ocr, TokenStorage $tss)
     {
         parent::__construct($name, $templating);
-        $this->em = $em;
+        $this->ocr = $ocr;
+        $this->tss = $tss;
     }
 
     /**
-     * Execute.
-     *
      * @param BlockContextInterface $blockContext
      * @param Response|null         $response
      *
@@ -53,13 +57,23 @@ class ContactMessageBlock extends AbstractBlockService
         // merge settings
         $settings = $blockContext->getSettings();
 
+        $backgroundColor = 'bg-green';
+        $content = '<p>Tots els operaris tenen les revisions al dia</p>';
+
+        $opertaorsInvailids = $this->ocr->getItemsInvalid();
+        if (count($opertaorsInvailids) > 0) {
+            $backgroundColor = 'bg-red';
+            $content = '<p>'.count($opertaorsInvailids).' operaris tenen revisions caducades</p>';
+        }
+
         return $this->renderResponse(
             $blockContext->getTemplate(),
             array(
                 'block' => $blockContext->getBlock(),
                 'settings' => $settings,
                 'title' => 'Notificacions',
-                'pendingMessages' => $this->em->getRepository('AppBundle:ContactMessage')->getPendingMessagesAmount(),
+                'background' => $backgroundColor,
+                'content' => $content,
             ),
             $response
         );
@@ -72,7 +86,7 @@ class ContactMessageBlock extends AbstractBlockService
      */
     public function getName()
     {
-        return 'pending_messages';
+        return 'operator_checking';
     }
 
     /**
@@ -83,7 +97,7 @@ class ContactMessageBlock extends AbstractBlockService
         $resolver->setDefaults(array(
             'title' => 'Resum',
             'content' => 'Default content',
-            'template' => ':Admin/Block:contact_message.html.twig',
+            'template' => ':Admin/Block:operator_checking.html.twig',
         ));
     }
 }
