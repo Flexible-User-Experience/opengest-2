4b2182a527702a89d9cd0629793ef6451fbcb43f
Wils Iglesias <wiglesias83@gmail.com>
2017-10-16 10:35:09 +0200

    add new attribute in entity

--- src/AppBundle/Entity/Vehicle.php
+++ src/AppBundle/Entity/Vehicle.php
@@ -23,7 +23,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  * @ORM\Entity(repositoryClass="AppBundle\Repository\VehicleRepository")
  * @ORM\Table(name="vehicle")
  * @Vich\Uploadable()
- * @UniqueEntity({"name"})
+ * @UniqueEntity({"name", "vehicleRegistrationNumber"})
  */
 class Vehicle extends AbstractBase
 {
@@ -39,6 +39,13 @@ class Vehicle extends AbstractBase
      * @Gedmo\Slug(fields={"name"})
      */
     private $slug;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     */
+    private $vehicleRegistrationNumber;
 
     /**
      * @var VehicleCategory
@@ -110,6 +117,24 @@ class Vehicle extends AbstractBase
     /**
      * Methods.
      */
+
+    /**
+     * @return string
+     */
+    public function getVehicleRegistrationNumber()
+    {
+        return $this->vehicleRegistrationNumber;
+    }
+
+    /**
+     * @param string $vehicleRegistrationNumber
+     * @return Vehicle
+     */
+    public function setVehicleRegistrationNumber($vehicleRegistrationNumber)
+    {
+        $this->vehicleRegistrationNumber = $vehicleRegistrationNumber;
+        return $this;
+    }
 
     /**
      * @return VehicleCategory
