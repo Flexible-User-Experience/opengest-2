bfce37e3a004bfad663b6b5304c265ae224d5e49
Wils Iglesias <wiglesias83@gmail.com>
2017-11-22 19:05:07 +0100

    improve Import Operator Command

--- src/AppBundle/Command/ImportOperatorCommand.php
+++ src/AppBundle/Command/ImportOperatorCommand.php
@@ -54,57 +54,57 @@ class ImportOperatorCommand extends AbstractBaseCommand
             $registrationDate = \DateTime::createFromFormat('Y-m-d', $this->readColumn(17, $row));
 
             $profilePhotoImage = $this->readColumn(8, $row);
-            if ($profilePhotoImage != '\\N') {
+            if (!is_null($profilePhotoImage)) {
                 $profilePhotoImage = explode('/', $profilePhotoImage);
             }
 
             $taxIdentityNumberImage = $this->readColumn(18, $row);
-            if ($taxIdentityNumberImage != '\\N') {
+            if (!is_null($taxIdentityNumberImage)) {
                 $taxIdentityNumberImage = explode('/', $taxIdentityNumberImage);
             }
 
             $drivingLicenseImg = $this->readColumn(19, $row);
-            if ($drivingLicenseImg != '\\N') {
+            if (!is_null($drivingLicenseImg)) {
                 $drivingLicenseImg = explode('/', $drivingLicenseImg);
             }
 
             $cranesOperatorLicenseImg = $this->readColumn(20, $row);
-            if ($cranesOperatorLicenseImg != '\\N') {
+            if (!is_null($cranesOperatorLicenseImg)) {
                 $drivingLicenseImg = explode('/', $cranesOperatorLicenseImg);
             }
 
             $medicalCheckImg = $this->readColumn(21, $row);
-            if ($medicalCheckImg != '\\N') {
+            if (!is_null($medicalCheckImg)) {
                 $medicalCheckImg = explode('/', $medicalCheckImg);
             }
 
             $episImg = $this->readColumn(41, $row);
-            if ($episImg != '\\N') {
+            if (!is_null($episImg)) {
                 $episImg = explode('/', $episImg);
             }
 
             $trainingDocImg = $this->readColumn(43, $row);
-            if ($trainingDocImg != '\\N') {
+            if (!is_null($trainingDocImg)) {
                 $trainingDocImg = explode('/', $trainingDocImg);
             }
 
             $informationImg = $this->readColumn(44, $row);
-            if ($informationImg != '\\N') {
+            if (!is_null($informationImg)) {
                 $informationImg = explode('/', $informationImg);
             }
 
             $useOfMachineryAuthorizationImg = $this->readColumn(45, $row);
-            if ($useOfMachineryAuthorizationImg != '\\N') {
+            if (!is_null($useOfMachineryAuthorizationImg)) {
                 $useOfMachineryAuthorizationImg = explode('/', $useOfMachineryAuthorizationImg);
             }
 
             $dischargeSocialSecurityImg = $this->readColumn(46, $row);
-            if ($dischargeSocialSecurityImg != '\\N') {
+            if (!is_null($dischargeSocialSecurityImg)) {
                 $dischargeSocialSecurityImg = explode('/', $dischargeSocialSecurityImg);
             }
 
             $employmentContractImg = $this->readColumn(47, $row);
-            if ($employmentContractImg != '\\N') {
+            if (!is_null($employmentContractImg)) {
                 $employmentContractImg = explode('/', $employmentContractImg);
             }
 
@@ -169,7 +169,7 @@ class ImportOperatorCommand extends AbstractBaseCommand
                     ->setTShirtSize($this->readColumn(29, $row))
                     ->setPantSize($this->readColumn(30, $row))
                     ->setWorkingDressSize($this->readColumn(31, $row))
-                    ->setBancAccountNumber(($this->readColumn(52, $row) != '\\N' ? $this->readColumn(52, $row).'-' : '').$this->readColumn(33, $row).'-'.$this->readColumn(34, $row).'-'.$this->readColumn(35, $row).'-'.$this->readColumn(36, $row))
+                    ->setBancAccountNumber(!is_null(($this->readColumn(52, $row)) ? $this->readColumn(52, $row).'-' : '').$this->readColumn(33, $row).'-'.$this->readColumn(34, $row).'-'.$this->readColumn(35, $row).'-'.$this->readColumn(36, $row))
                     ->setSocialSecurityNumber($this->readColumn(37, $row))
                     ->setEpisImage(is_array($episImg) ? $episImg[1] : null)
                     ->setTrainingDocumentImage(is_array($trainingDocImg) ? $trainingDocImg[1] : null)
@@ -179,7 +179,7 @@ class ImportOperatorCommand extends AbstractBaseCommand
                     ->setEmploymentContractImage(is_array($employmentContractImg) ? $employmentContractImg[1] : null)
                 ;
 
-                if ($this->readColumn(38, $row) != '\\N') {
+                if (!is_null($this->readColumn(38, $row))) {
                     $operator->setHourCost($this->readColumn(38, $row));
                 }
 
