cf09e7afc7644bebf1699c2fd2f19847b64fcaab
Ruben Hierro <rubenhierro@gmail.com>
2018-07-31 13:14:46 +0200

    Add Partner entity

---
+++ src/AppBundle/Repository/PartnerRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Class PartnerRepository.
+ *
+ * @author Rubèn Hierro <info@rubenhierro.com>
+ */
+class PartnerRepository extends EntityRepository
+{
+}
