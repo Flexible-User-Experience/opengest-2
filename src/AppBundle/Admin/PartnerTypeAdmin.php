275b0bbf8a75d9830ba5f33086b6182296b8efcb
Ruben Hierro <rubenhierro@gmail.com>
2018-07-31 09:44:57 +0200

    ParterType admin

---
+++ src/AppBundle/Admin/PartnerTypeAdmin.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+/**
+ * Class PartnerTypeAdmin.
+ *
+ * @category Admin
+ *
+ * @author   Rubèn Hierro <info@rubenhierro.com>
+ */
+class PartnerTypeAdmin extends AbstractBaseAdmin
+{
+    protected $classnameLabel = 'Tercers tipus';
+    protected $baseRoutePattern = 'tercers/tipus';
+    protected $datagridValues = array(
+        '_sort_by' => 'name',
+        '_sort_order' => 'asc',
+    );
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->with('General', $this->getFormMdSuccessBoxArray(6))
+                ->add(
+                    'name',
+                    null,
+                    array(
+                        'label' => 'Nom',
+                        'required' => true,
+                    )
+                )
+                ->add(
+                    'description',
+                    null,
+                    array(
+                        'label' => 'Descripció',
+                        'required' => true,
+                    )
+                )
+                ->add(
+                    'account',
+                    null,
+                    array(
+                        'label' => 'Compte',
+                        'required' => false,
+                    )
+                )
+            ->end()
+        ;
+    }
+
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add(
+                'name',
+                null,
+                array(
+                    'label' => 'Nom',
+                )
+            )
+            ->add(
+                'account',
+                null,
+                array(
+                    'label' => 'Compte',
+                )
+            )
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        unset($this->listModes['mosaic']);
+        $listMapper
+            ->add(
+                'name',
+                null,
+                array(
+                    'label' => 'Nom',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'description',
+                null,
+                array(
+                    'label' => 'Descripció',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'account',
+                null,
+                array(
+                    'label' => 'Compte',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                '_action',
+                'actions',
+                array(
+                    'actions' => array(
+                        'show' => array('template' => '::Admin/Buttons/list__action_show_button.html.twig'),
+                        'edit' => array('template' => '::Admin/Buttons/list__action_edit_button.html.twig'),
+                        'delete' => array('template' => '::Admin/Buttons/list__action_delete_button.html.twig'),
+                    ),
+                    'label' => 'Accions',
+                )
+            )
+        ;
+    }
+}
