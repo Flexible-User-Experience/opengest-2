09a66e185e45245aeb0370428f150679cf5615c7
Ruben Hierro <rubenhierro@gmail.com>
2018-08-03 16:52:04 +0200

    Improve VehicleDigitalTachographAdminController

--- src/AppBundle/Controller/Admin/VehicleDigitalTachographAdminController.php
+++ src/AppBundle/Controller/Admin/VehicleDigitalTachographAdminController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller\Admin;
 
 use AppBundle\Entity\VehicleDigitalTachograph;
 use AppBundle\Service\GuardService;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 /**
  * Class VehicleDigitalTachographAdminController.
@@ -13,7 +14,7 @@ class VehicleDigitalTachographAdminController extends BaseAdminController
     /**
      * @param null $id
      *
-     * @return \Symfony\Component\HttpFoundation\StreamedResponse
+     * @return StreamedResponse
      */
     public function downloadAction($id = null)
     {
@@ -21,19 +22,37 @@ class VehicleDigitalTachographAdminController extends BaseAdminController
         $id = $request->get($this->admin->getIdParameter());
 
         /** @var VehicleDigitalTachograph $tachograph */
-        $tachograph = $this->admin->getObject($id);
-        if (!$tachograph) {
+        $vehicleDigitalTachograph = $this->admin->getObject($id);
+        if (!$vehicleDigitalTachograph) {
             throw $this->createNotFoundException(sprintf('unable to find the object with id: %s', $id));
         }
 
         /** @var GuardService $guardService */
         $guardService = $this->container->get('app.guard_service');
-        if (!$guardService->isOwnVehicle($tachograph->getVehicle())) {
+        if (!$guardService->isOwnVehicle($vehicleDigitalTachograph->getVehicle())) {
             throw $this->createNotFoundException(sprintf('forbidden object with id: %s', $id));
         }
 
         $downloadHandler = $this->container->get('vich_uploader.download_handler');
 
-        return $downloadHandler->downloadObject($tachograph, 'uploadedFile');
+        return $downloadHandler->downloadObject($vehicleDigitalTachograph, 'uploadedFile');
+    }
+
+    public function editAction($id = null)
+    {
+        $request = $this->getRequest();
+        $id = $request->get($this->admin->getIdParameter());
+
+        $vehicleDigitalTachograph = $this->admin->getObject($id);
+        if (!$vehicleDigitalTachograph) {
+            throw $this->createNotFoundException(sprintf('unable to find the object wirh id %s', $id));
+        }
+
+        $guardService = $this->container->get('app.guard_service');
+        if (!$guardService->isOwnVehicle($vehicleDigitalTachograph->getVehicle())) {
+            throw $this->createNotFoundException('forbidden object wirh id %s', $id);
+        }
+
+        return parent::editAction($id);
     }
 }
