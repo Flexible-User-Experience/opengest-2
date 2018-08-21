043e90cb5bb78c8474e2270e00cce3210b79ba48
Ruben Hierro <rubenhierro@gmail.com>
2018-08-21 18:21:31 +0200

    Improve PartnerOrderAdmin list query

--- src/AppBundle/Admin/PartnerOrderAdmin.php
+++ src/AppBundle/Admin/PartnerOrderAdmin.php
@@ -104,7 +104,8 @@ class PartnerOrderAdmin extends AbstractBaseAdmin
         $queryBuilder = parent::createQuery($context);
         if (!$this->acs->isGranted(UserRolesEnum::ROLE_ADMIN)) {
             $queryBuilder
-                ->andWhere($queryBuilder->getRootAliases()[0].'.partner.enterprise = :enterprise')
+                ->join($queryBuilder->getRootAliases()[0].'.partner', 'pa')
+                ->andWhere('pa.enterprise = :enterprise')
                 ->setParameter('enterprise', $this->getUserLogedEnterprise())
             ;
         }
