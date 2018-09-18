ffa4a318500e65d4ae1a1470bf906b8531e1f17d
Ruben Hierro <rubenhierro@gmail.com>
2018-09-18 12:21:18 +0200

    Fix EnterpriseHolidays entity

--- src/AppBundle/Entity/EnterpriseHolidays.php
+++ src/AppBundle/Entity/EnterpriseHolidays.php
@@ -29,7 +29,7 @@ class EnterpriseHolidays extends AbstractBase
     /**
      * @var string
      *
-     * @ORM\Column(type="string", nullable=true")
+     * @ORM\Column(type="string", nullable=true)
      */
     private $name;
 
