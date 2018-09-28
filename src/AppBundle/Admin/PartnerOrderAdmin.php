c38045bf5889335629a0e7dfdc917dc067b7c0e4
Ruben Hierro <rubenhierro@gmail.com>
2018-09-28 12:02:31 +0200

    Improve PartnerOrderAdmin

--- src/AppBundle/Admin/PartnerOrderAdmin.php
+++ src/AppBundle/Admin/PartnerOrderAdmin.php
@@ -8,7 +8,7 @@ use Doctrine\ORM\QueryBuilder;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Sonata\AdminBundle\Form\Type\ModelAutocompleteType;
 
 /**
  * Class PartnerOrderAdmin.
@@ -35,12 +35,20 @@ class PartnerOrderAdmin extends AbstractBaseAdmin
             ->with('General', $this->getFormMdSuccessBoxArray(4))
                 ->add(
                     'partner',
-                    EntityType::class,
+                    ModelAutocompleteType::class,
                     array(
-                        'class' => Partner::class,
+                        'property' => 'name',
                         'label' => 'Tercer',
                         'required' => true,
-                        'query_builder' => $this->rm->getPartnerRepository()->getFilteredByEnterpriseEnabledSortedByNameQB($this->getUserLogedEnterprise()),
+                        'callback' => function ($admin, $property, $value) {
+                            $datagrid = $admin->getDatagrid();
+                            $queryBuilder = $datagrid->getQuery();
+                            $queryBuilder
+                                ->andWhere($queryBuilder->getRootAliases()[0].'.enterprise = :enterprise')
+                                ->setParameter('enterprise', $this->getUserLogedEnterprise())
+                            ;
+                            $datagrid->setValue($property, null, $value);
+                        },
                     )
                 )
                 ->add(
@@ -71,8 +79,11 @@ class PartnerOrderAdmin extends AbstractBaseAdmin
         $datagridMapper
             ->add(
                 'partner',
+                'doctrine_orm_model_autocomplete',
+                array(),
                 null,
                 array(
+                    'property' => 'name',
                     'label' => 'Tercer',
                 )
             )
