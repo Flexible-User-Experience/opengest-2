6d3f3c1a8e09a1ff5ff3d2711ecb8357a8bed691
Ruben Hierro <rubenhierro@gmail.com>
2018-10-04 12:46:01 +0200

    CollectionDocumentTypeAdmin

---
+++ src/AppBundle/Controller/Admin/CollectionDocumentTypeAdminController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Controller\Admin;
+
+use AppBundle\Entity\CollectionDocumentType;
+use AppBundle\Service\GuardService;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class CollectionDocumentTypeAdminController.
+ */
+class CollectionDocumentTypeAdminController extends BaseAdminController
+{
+    /**
+     * @param int|null $id
+     *
+     * @return RedirectResponse|Response
+     */
+    public function editAction($id = null)
+    {
+        $request = $this->getRequest();
+        $id = $request->get($this->admin->getIdParameter());
+
+        /** @var CollectionDocumentType $collectionDocumentType */
+        $collectionDocumentType = $this->admin->getObject($id);
+        if (!$collectionDocumentType) {
+            throw $this->createNotFoundException(sprintf('unable to find the object with id: %s', $id));
+        }
+        /** @var GuardService $guardService */
+        $guardService = $this->container->get('app.guard_service');
+        if (!$guardService->isOwnEnterprise($collectionDocumentType->getEnterprise())) {
+            throw $this->createNotFoundException(sprintf('forbidden object with id: %s', $id));
+        }
+
+        return parent::editAction($id);
+    }
+}
