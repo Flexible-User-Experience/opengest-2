3c1ecdd0c8e8db9ec5016e811d9a51637d06cf65
Ruben Hierro <rubenhierro@gmail.com>
2018-10-05 12:02:22 +0200

    Add SaleInvoiceSeries entity

---
+++ src/AppBundle/Repository/SaleInvoiceSeriesRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Class     SaleInvoiceSeriesRepository.
+ *
+ * @category Repository
+ */
+class SaleInvoiceSeriesRepository extends EntityRepository
+{
+}
