7110a1e88ab72e46751592a758a2ceb8ea8832bd
Ruben Hierro <rubenhierro@gmail.com>
2018-11-26 18:32:02 +0100

    PartnerUnableDays Admin

---
+++ src/AppBundle/Admin/PartnerUnableDaysAdmin.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Doctrine\ORM\QueryBuilder;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Form\Type\ModelAutocompleteType;
+use Sonata\CoreBundle\Form\Type\DatePickerType;
+
+/**
+ * Class PartnerUnableDaysAdmin.
+ *
+ * @category Admin
+ *
+ * @author   Rubèn Hierro <info@rubenhierro.com>
+ */
+class PartnerUnableDaysAdmin extends AbstractBaseAdmin
+{
+    protected $classnameLabel = 'Tercers dies inhàbils';
+    protected $baseRoutePattern = 'tercers/dies-inhabils';
+    protected $datagridValues = array(
+        '_sort_by' => 'begin',
+        '_sort_order' => 'desc',
+    );
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+
+            ->with('General', $this->getFormMdSuccessBoxArray(4))
+
+            ->add(
+                'partner',
+                ModelAutocompleteType::class,
+                array(
+                    'property' => 'name',
+                    'label' => 'Tercer',
+                    'required' => true,
+                    'callback' => function ($admin, $property, $value) {
+                        $datagrid = $admin->getDatagrid();
+                        $queryBuilder = $datagrid->getQuery();
+                        $queryBuilder
+                            ->andWhere($queryBuilder->getRootAliases()[0].'.enterprise = :enterprise')
+                            ->setParameter('enterprise', $this->getUserLogedEnterprise())
+                        ;
+                        $datagrid->setValue($property, null, $value);
+                    },
+                )
+            )
+            ->add(
+                'begin',
+                DatePickerType::class,
+                array(
+                    'label' => 'Data inici',
+                    'format' => 'd/m/Y',
+                    'required' => true,
+                    'dp_default_date' => (new \DateTime())->format('d/m/Y'),
+                )
+            )
+            ->add(
+                'end',
+                DatePickerType::class,
+                array(
+                    'label' => 'Data fi',
+                    'format' => 'd/m/Y',
+                    'required' => true,
+                    'dp_default_date' => (new \DateTime())->format('d/m/Y'),
+                )
+            )
+        ;
+    }
+
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        if ($this->acs->isGranted(UserRolesEnum::ROLE_ADMIN)) {
+            $datagridMapper
+                ->add(
+                    'partner.enterprise',
+                    null,
+                    array(
+                        'label' => 'Empresa',
+                    )
+                )
+            ;
+        }
+        $datagridMapper
+            ->add(
+                'partner',
+                'doctrine_orm_model_autocomplete',
+                array(
+                    'label' => 'Partner',
+                ),
+                null,
+                array(
+                    'property' => 'name',
+                )
+            )
+            ->add(
+                'begin',
+                'doctrine_orm_date',
+                array(
+                    'label' => 'Data inici',
+                    'field_type' => 'sonata_type_date_picker',
+                )
+            )
+            ->add(
+                'end',
+                'doctrine_orm_date',
+                array(
+                    'label' => 'Data fi',
+                    'field_type' => 'sonata_type_date_picker',
+                )
+            )
+
+        ;
+    }
+
+    /**
+     * @param string $context
+     *
+     * @return QueryBuilder
+     */
+    public function createQuery($context = 'list')
+    {
+        /** @var QueryBuilder $queryBuilder */
+        $queryBuilder = parent::createQuery($context);
+        if (!$this->acs->isGranted(UserRolesEnum::ROLE_ADMIN)) {
+            $queryBuilder
+                ->andWhere($queryBuilder->getRootAliases()[0].'.partner.enterprise = :enterprise')
+                ->setParameter('enterprise', $this->getUserLogedEnterprise())
+            ;
+        }
+
+        return $queryBuilder;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        unset($this->listModes['mosaic']);
+        if ($this->acs->isGranted(UserRolesEnum::ROLE_ADMIN)) {
+            $listMapper
+                ->add(
+                    'partner.enterprise',
+                    null,
+                    array(
+                        'label' => 'Empresa',
+                    )
+                )
+            ;
+        }
+
+        $listMapper
+            ->add(
+                'partner',
+                null,
+                array(
+                    'label' => 'Tercer',
+                    'editable' => false,
+                )
+            )
+            ->add(
+                'begin',
+                null,
+                array(
+                    'label' => 'Data inici',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'end',
+                null,
+                array(
+                    'label' => 'Data fi',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                '_action',
+                'actions',
+                array(
+                    'actions' => array(
+                        'show' => array('template' => '::Admin/Buttons/list__action_show_button.html.twig'),
+                        'edit' => array('template' => '::Admin/Buttons/list__action_edit_button.html.twig'),
+                        'delete' => array('template' => '::Admin/Buttons/list__action_delete_button.html.twig'),
+                    ),
+                    'label' => 'Accions',
+                )
+            )
+        ;
+    }
+}
