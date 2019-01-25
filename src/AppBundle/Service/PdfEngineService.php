e8f9bab87dcda848eccb448c0779af138c4d81b4
David Romaní <david@flux.cat>
2019-01-25 11:22:14 +0100

    improve PdfEngineService

--- src/AppBundle/Service/PdfEngineService.php
+++ src/AppBundle/Service/PdfEngineService.php
@@ -73,6 +73,34 @@ class PdfEngineService
         $this->engine->SetSubject($this->subject);
         $this->engine->SetKeywords('TCPDF, PDF, '.$this->author);
 
+        // set default header data
+        $this->engine->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 001', PDF_HEADER_STRING, array(0, 64, 255), array(0, 64, 128));
+        $this->engine->setFooterData(array(0, 64, 0), array(0, 64, 128));
+
+        // set header and footer fonts
+        $this->engine->setHeaderFont(array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+        $this->engine->setFooterFont(array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+
+        // set default monospaced font
+        $this->engine->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+        // set margins
+        $this->engine->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+        $this->engine->SetHeaderMargin(PDF_MARGIN_HEADER);
+        $this->engine->SetFooterMargin(PDF_MARGIN_FOOTER);
+
+        // set auto page breaks
+        $this->engine->SetAutoPageBreak(true, PDF_MARGIN_BOTTOM);
+
+        // set image scale factor
+        $this->engine->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+        // set default font subsetting mode
+        $this->engine->setFontSubsetting(true);
+
+        // set font
+        $this->engine->SetFont('dejavusans', '', 14, '', true);
+
         return $this->engine;
     }
 }
