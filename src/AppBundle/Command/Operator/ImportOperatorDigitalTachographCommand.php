b4c2556fd7a823e63ec61439e3a48449dce77785
David Romaní <david@flux.cat>
2019-02-04 18:34:42 +0100

    improve some import relationship references 3

--- src/AppBundle/Command/Operator/ImportOperatorDigitalTachographCommand.php
+++ src/AppBundle/Command/Operator/ImportOperatorDigitalTachographCommand.php
@@ -51,7 +51,7 @@ class ImportOperatorDigitalTachographCommand extends AbstractBaseCommand
         $newRecords = 0;
         $errors = 0;
         while (false != ($row = $this->readRow($fr))) {
-            $operator = $this->em->getRepository('AppBundle:Operator\Operator')->findOneBy(['id' => $this->readColumn(1, $row)]);
+            $operator = $this->em->getRepository('AppBundle:Operator\Operator')->findOneBy(['taxIdentificationNumber' => $this->readColumn(4, $row)]);
             $date = \DateTime::createFromFormat('Y-m-d H:i:s', $this->readColumn(2, $row));
             $file = $this->readColumn(3, $row);
             if ($operator && $date && $file) {
