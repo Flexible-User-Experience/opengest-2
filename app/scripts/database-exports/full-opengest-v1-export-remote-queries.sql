3f7d9b8dc3d964bceadbe97abedd41403d4d2f66
David Romaní <david@flux.cat>
2019-02-25 16:08:24 +0100

    fix partners csv filename export

--- app/scripts/database-exports/full-opengest-v1-export-remote-queries.sql
+++ app/scripts/database-exports/full-opengest-v1-export-remote-queries.sql
@@ -136,7 +136,7 @@ LINES TERMINATED BY '\n'
 FROM opengest.Tipos_terceros TT;
 
 SELECT T.*, E.cif_nif AS E_cif_nif, TT.nombre AS TT_nombre, CT.nombre AS CT_nombre, CUTR.nombre AS CUTR_nombre
-INTO OUTFILE '/var/lib/mysql-files/partner.csv'
+INTO OUTFILE '/var/lib/mysql-files/partners.csv'
 FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"'
 ESCAPED BY '\\'
